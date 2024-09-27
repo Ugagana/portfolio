424891d6ce165af280190eee55fbc489d6790624
GaganaUKumar <gagana.u@yahoo.com>
2024-09-27 13:59:18 -0400

    skills

--- src/data/skills.jsx
+++ src/data/skills.jsx
@@ -1,7 +1,7 @@
 
 import ReactJs, { HTML, CSS, MongoDB,
-    Python,  
-     SASS,
+    Python, Heroku,
+     SASS,PostMan,JQuery,
     MySQL,  PostGreSql,
     VSCode, Git, GitHub, Bash, 
      Kubernets, 
@@ -9,58 +9,65 @@ import ReactJs, { HTML, CSS, MongoDB,
 
 
 const skills = [
+
     {
         id: 1,
-        icon: <HTML />,
-        name: "HTML",
+        icon: <Python />,
+        name: "Python",
         style: {
-            shadow: "shadow-[#ca4828]",
-            cover: "from-black to-[#ca4828]  rounded-b-md",
+            shadow: "shadow-[#F34F29]",
+            cover: "from-black to-[#F34F29] rounded-b-md",
             opacity: "opacity-80"
+
         }
     },
     {
         id: 2,
         icon: <CSS />,
-        name: "CSS",
+        name: "SAS",
         style: {
             shadow: "shadow-[#1d64c7]",
             cover: "from-black to-[#1d64c7] rounded-b-md",
             opacity: "opacity-80"
         }
     },
-    
-  
+    {
+        id: 3,
+        icon: <MongoDB />,
+        name: "R",
+        style: {
+            shadow: "shadow-[#5c5a5a]",
+            cover: "from-black to-[#5c5a5a] rounded-b-md",
+            opacity: "opacity-80"
+            
+
+        }
+    },
+    {
+        id: 4,
+        icon: <SASS />,
+        name: "PowerBI",
+        style: {
+            shadow: "shadow-[#ddbe4e]",
+            cover: "from-black to-[#ddbe4e] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
+    {
+        id: 11,
+        icon: <Bash />,
+        name: "Tableau",
+        style: {
+            shadow: "shadow-[#4dbc85]",
+            cover: "from-black to-[#4dbc85] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
+
     {
         id: 5,
-        icon: <MongoDB />,
-        name: "MongoDB",
-        style: {
-            shadow: "shadow-[#4FAA41]",
-            cover: "from-black to-[#4FAA41] rounded-b-md",
-            opacity: "opacity-80"
-
-        }
-    },
-   
-  
-
-  
-   
-    {
-        id: 12,
-        icon: <SASS />,
-        name: "SASS",
-        style: {
-            shadow: "shadow-[#CC6699]",
-            cover: "from-black to-[#CC6699] rounded-b-md",
-            opacity: "opacity-80"
-
-        }
-    },
-
-    {
-        id: 14,
         icon: <MySQL />,
         name: "MySQL",
         style: {
@@ -71,29 +78,50 @@ const skills = [
         }
     },
     {
-        id: 33,
+        id: 6,
         icon: <PostGreSql />,
         name: "PostgreSql",
         style: {
-            shadow: "shadow-[#336791]",
-            cover: "from-black to-[#336791] rounded-b-md",
+            shadow: "shadow-[#4FAA41]",
+            cover: "from-black to-[#4FAA41] rounded-b-md",
             opacity: "opacity-80"
 
         }
     },
+    {
+        id: 9,
+        icon: <HTML />,
+        name: "HTML",
+        style: {
+            shadow: "shadow-[#ca4828]",
+            cover: "from-black to-[#ca4828]  rounded-b-md",
+            opacity: "opacity-80"
+        }
+    },
+    
+  
+    
+   
+  
+
+  
+   
+  
+
+    
     
     {
-        id: 16,
+        id: 7,
         icon: <Git />,
         name: "Git",
         style: {
-            shadow: "shadow-[#F34F29]",
-            cover: "from-black to-[#F34F29] rounded-b-md",
+            shadow: "shadow-[#4dbc85]",
+            cover: "from-black to-[#4dbc85] rounded-b-md",
             opacity: "opacity-80"
         }
     },
     {
-        id: 17,
+        id: 8,
         icon: <GitHub />,
         name: "GitHub",
         style: {
@@ -104,20 +132,10 @@ const skills = [
     },
 
     
-    {
-        id: 20,
-        icon: <Python />,
-        name: "Python",
-        style: {
-            shadow: "shadow-[#ddbe4e]",
-            cover: "from-black to-[#ddbe4e] rounded-b-md",
-            opacity: "opacity-80"
-
-        }
-    },
+  
   
     {
-        id: 25,
+        id: 10,
         icon: <VSCode />,
         name: "VS Code",
         style: {
@@ -127,22 +145,25 @@ const skills = [
 
         }
     },
+   
+
     {
-        id: 26,
-        icon: <Bash />,
-        name: "Bash",
+        id: 12,
+        icon: <Kubernets />,
+        name: "Kubernets",
         style: {
-            shadow: "shadow-[#4dbc85]",
-            cover: "from-black to-[#4dbc85] rounded-b-md",
+            shadow: "shadow-[#ddbe4e]",
+            cover: "from-black to-[#ddbe4e] rounded-b-md",
             opacity: "opacity-80"
 
         }
     },
-
+   
+   
     {
-        id: 32,
-        icon: <Kubernets />,
-        name: "Kubernets",
+        id: 13,
+        icon: <PostMan />,
+        name: "Snowflake",
         style: {
             shadow: "shadow-[#326DE6]",
             cover: "from-black to-[#326DE6] rounded-b-md",
@@ -150,9 +171,28 @@ const skills = [
 
         }
     },
-   
-   
+    {
+        id: 14,
+        icon: <JQuery />,
+        name: "Databricks",
+        style: {
+            shadow: "shadow-[#F34F29]",
+            cover: "from-black to-[#F34F29] rounded-b-md",
+            opacity: "opacity-80"
 
+        }
+    },
+    {
+        id: 15,
+        icon: <Heroku />,
+        name: "Microsoft",
+        style: {
+            shadow: "shadow-[#4FAA41]",
+            cover: "from-black to-[#4FAA41] rounded-b-md",
+            opacity: "opacity-80"
+
+        }
+    },
 
 ]
 export default skills;
